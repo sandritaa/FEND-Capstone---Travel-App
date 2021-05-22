520956468641112a1d7eb85b2a1a96b17cfc214b
sandrita <sandra@Sandras-Air.attlocal.net>
2021-05-21 17:20:39 -0700

    Create APIs and functions

--- src/client folder/js/app.js
+++ src/client folder/js/app.js
@@ -1,29 +1,28 @@
-async function getFromServer() { //attribute names assigned in function
+async function getFromServer() {
     const backEnd = await fetch('/getroute128'); 
     const data = await backEnd.json();
     return data;
 }
 
 // front end to back end (call post function we created)
-const idx = getFromServer() 
+// const idx = getFromServer() 
 
 /* POST - Async Funtion*/
 
 
-async function postWaitTime(city, date, weather, picture) { //attribute names assigned in function
-    let myTripAllTogether = { //crate object using the key (name) assosnged in server.js and pair with values in that we created in our function attributes. 
+async function postWaitTime(city, date, weather, picture) { 
+    let myTripAllTogether = { 
         destinationCity: city,
         arrivalDate: date,
         destinationWeather: weather,
         destinationPic: picture
     }
     
-
-    await fetch('/postroute136', { //our route name that we created on server.js 
+    await fetch('/postroute136', { 
         method: 'POST',
         credentials: 'same-origin',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(myTripAllTogether)//what the client side is sending to the server (this body extension can be pulled from server.js file)
+        body: JSON.stringify(myTripAllTogether)
     }) 
    
 }
@@ -33,36 +32,72 @@ async function getDataFromBackEnd() {
         method: 'GET'  
 })
 }
-debugger;
+
 
 
 /* GET - Async Function to API*/
 
-async function getWeatherApi(urlUno, keyUno, zipCodeUno) { 
-    const temperatureInCity = await fetch(urlUno+zipCodeUno+'&appid='+keyUno); // here we use as arguments the three parts of information that we need. The word &appid is requried by the API. We use our own arguments here.
-    const temperature = await temperatureInCity.json(); // here we are using .json to translate our JS response in this case (temperatureInCity) to get data.
-    return temperature.main.temp //return this item within our function to have access to this variable outside of the function. This will be used indirectly in the postWaitTime function. 
+
+//GEONAMES API
+const geoURL = 'http://api.geonames.org/';
+const geoQuery ='searchJSON?formatted=true&q=';
+const geoKey = '&username=sandrita';
+
+// http://api.geonames.org/searchJSON?formatted=true&q=florence&username=sandrita
+
+async function geoApi(urlGeo,queryGeo,locationGeo,keyGeo) { 
+    const cityFetch = await fetch(urlGeo+queryGeo+locationGeo+keyGeo);  
+    const cityGeo = await cityFetch.json(); //converting from JSON to an object
+    const cordsGeo = {
+        latGeo:cityGeo.geonames[0].lat,
+        lngGeo:cityGeo.geonames[0].lng,
+    }
+    return cordsGeo;
+
+    // return cityGeo.main.temp; 
 };
 
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+geoApi(geoURL,geoQuery,'paris',geoKey) //only for testing
 
 
-/*  API Global Variables*/ 
-
-//GEONAMES API
-const geoURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';// Link is provided by API
-const geoKey = '278a4bb0ed3e1dc04317050dfdf8f145';// Number key is provided by API   
+// let a = fetch('http://api.geonames.org/searchJSON?formatted=true&q=florence&username=sandrita')
+// http://api.geonames.org/
+// searchJSON?formatted=true&q=
+// florence
+// sandrita
 
 //WEATHERBIT API
-const weatherURL = 
-const weatherKey =    
+const weatherURL = 'https://api.weatherbit.io/v2.0/forecast/daily';
+const weatherKey = '925a0421821c4963b0c150342f7e173b';
 
-//PINABAY API
-const pinaURL = 
-const pinaKey = 
+// https://api.weatherbit.io/v2.0/forecast/daily?&lat=38.123&lon=-78.543,NC&key=API_KEY
 
+
+async function weatherApi(urlWeather, keyWeather, geoApi) { 
+    const weatherFetch = await fetch(urlWeather+keyWeather+geoApi); 
+    const weatherBit = await weatherFetch.json();
+    return weatherBit
+    // return weatherBit.main.temp 
+};
+
+
+// PINABAY API
+const pinaURL = 'https://pixabay.com/api/'
+const pinaKey = '?key=21697550-400e4664c2106af48cb147104&q='
+
+// https://pixabay.com/api/?key=21697550-400e4664c2106af48cb147104&q=yellow+flowers&image_type=photo
+
+async function pictureApi(urlPina,keyPina,cityPina,typePina) { 
+    const pictureFetch = await fetch(urlPina+keyPina+cityPina+typePina); 
+    debugger;
+    const pictureBay = await pictureFetch.json(); 
+    return pictureBay
+    //  return pictureBay.main.temp 
+};
+
+pictureApi(pinaURL,pinaKey,'paris','&image_type=photo')
 
 /*Create a new date instance dynamically with JS*/
 
@@ -73,112 +108,21 @@ let date = day.getMonth()+'.'+ day.getDate()+'.'+ day.getFullYear(); //name only
 
 /*Creating Event Listener*/
 
-const generateListener = document.getElementById('generate');// Defining variable that will be used in event listener
+// const generateListener = document.getElementById('generate');// Defining variable that will be used in event listener
 
-generateListener.addEventListener("click", function(){ //Here we kick off our event listener
-    //HTML to front end
-    const city = document.getElementById('cityDestination')
-    const date = document.getElementById('departureDate').value; // Here we are pulling this information from our HTML, creating a variable where we will next the information and use it in the above function and event listener command.
-    const temperatureDos = getWeatherApi(baseURL, baseKey, zipcodeDos)// Create variable to nest getWeatherAPI info. Temprature variable only in app.js 
-    .then(function(zipcodeDos, temperatureDos, userMoodDos) {
-        postWaitTime(zipcodeDos, temperatureDos, userMoodDos)
-    });    
+// generateListener.addEventListener("click", function(){ //Here we kick off our event listener
+   
+//     const city = document.getElementById('cityDestination')
+//     const date = document.getElementById('arrivalDate').value; // Here we are pulling this information from our HTML, creating a variable where we will next the information and use it in the above function and event listener command.
+//     const temperatureDos = getWeatherApi(geoURL, geokey, geoQuery)// Create variable to nest getWeatherAPI info. Temprature variable only in app.js 
+//     .then(function(zipcodeDos, temperatureDos, userMoodDos) {
+//         postWaitTime(zipcodeDos, temperatureDos, userMoodDos)
+//     });    
 
-    // front end to back end (call post function we created)
-    const idx = getFromServer() 
-    // idx, as agreed in the app.get method in the server.js, will be an object with the following keys: fetcha, temperatura, sentimiento
+//     front end to back end (call post function we created)
+//     const idx = getFromServer() 
+//     idx, as agreed in the app.get method in the server.js, will be an object with the following keys: fetcha, temperatura, sentimiento
     
-    // front end to HTML (we still need to create this function)
-    updateHTML(idx)                                                                                                                                                                                                                                                                  
-}); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function sum(a,b){
-//     let c = a+b
-//     return c
-// }
-
-// let myValue = sum(2,3)
-
-// console.log(myValue)
-
-
-
-
-
-
-
-// js day (app.js name) = dia (function name ) = date (server name that we are promising our backend for storage )
-// js tura (app.js name) = temp (function name) = temperature (server name that we are promising our backend for storage )
-// js mood (app.js name) = feels (function name) = userMood (server name that we are promising our backend for storage )
-
-
-
-
-// postWaitTime(date,tura, mood)
-
-// function sum(a,b){
-//     return a+b
-// }
-
-// function multiply(a,b){
-//     return a*b
-// }
-
-
-// GOOD MORNING! I LOVE YOUUUUUU
-// let addition = sum(2,3)
-
-// let multiplication = multiply(4,5)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function printMyName() {
-//     return "Tom"
-// }
-
-// let name  = printMyName(); // -> Tom
-
-
-// async function printMyName() {
-//     return "Tom"
-// }
-
-// printMyName().then(alert) // -> Tom
-
-
-
-
-// function printMyName() {
-//     Promise.resolve(console.log("Tom"))
-// }
+//     front end to HTML (we still need to create this function)
+//     updateHTML(idx)                                                                                                                                                                                                                                                                  
+// }); 
